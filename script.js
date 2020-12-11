8d21c7f2dc67474a3ed42ef683b53b5776d40d3b
Nardin Lachowski <nardin@live.com>
2020-12-10 22:07:46 -0500

    corrected the ajax and click handler functionality

--- script.js
+++ script.js
@@ -4,36 +4,34 @@ var searchInput = "";
 //API related variables
 let apiKey = "2ba2c982b41349dab1c118f2767a1cd1";
 
-function searchRecipe () {
-    let queryURL = `https://api.spoonacular.com/recipes/search?apiKey=${apiKey}&number=4&query=${searchInput}`
-    
+function searchRecipe() {
+    let queryURL = `https://api.spoonacular.com/recipes/search?apiKey=${apiKey}&number=4&query=${searchInput}`;
+
     $.ajax({
         url: queryURL,
         method: "GET"
 
-    }).then(function (response){
-        console.log(response);
+    }).then(function (response) {
+        console.log(response); //to test API URL
     });
 }
 
 // Hide shopping list using close button
-$(".close-button").on("click", function() {
+$(".close-button").on("click", function () {
     $("aside").hide();
 });
 
 
 //Click Handler When Search is Submitted
-$(".searchRecipe").on("click", function(event){
+$(".button").on("click", function (event) {
     event.preventDefault();
 
-    var searchInput = (document.querySelector(".searchRecipe")).value.trim();
+    var searchInput = $(".searchRecipe").value;
     //if input is blank, return from funciton
-    if (searchInput == "") {
+    if (searchInput === "") {
         return;
     }
 
-    searchInput.text = "";
-
-    searchRecipe ();
+    searchRecipe();
 });
 
