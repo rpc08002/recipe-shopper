47aca4569c9ea64a659123129303fb13666f0cec
Nardin Lachowski <nardin@live.com>
2020-12-10 21:30:06 -0500

    added to js script to include click handler and ajax

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
+//Search input
+var searchInput = "";
+
+//API related variables
+let apiKey = "2ba2c982b41349dab1c118f2767a1cd1";
+
+function searchRecipe () {
+    let queryURL = `https://api.spoonacular.com/recipes/search?apiKey=${apiKey}&number=4&query=${searchInput}`
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+
+    }).then(function (response){
+        console.log(response);
+    });
+}
+
 // Hide shopping list using close button
 $(".close-button").on("click", function() {
     $("aside").hide();
 });
+
+
+//Click Handler When Search is Submitted
+$(".searchRecipe").on("click", function(event){
+    event.preventDefault();
+
+    var searchInput = (document.querySelector(".searchRecipe")).value.trim();
+    //if input is blank, return from funciton
+    if (searchInput == "") {
+        return;
+    }
+
+    searchInput.text = "";
+
+    searchRecipe ();
+});
+
